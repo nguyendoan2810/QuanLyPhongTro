212ae29469f6d021c3bc3d7c4e896f6b90f65197
Bnseaaaa <haibuiboy@gmail.com>
2025-09-25 11:06:06 +0700

    sua

--- QuanLyPhongTro/Areas/QuanLy/Controllers/DichVuController.cs
+++ QuanLyPhongTro/Areas/QuanLy/Controllers/DichVuController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyPhongTro.Models;
 
 namespace QuanLyPhongTro.Areas.QuanLy.Controllers
 {
+    [Area("QuanLy")]
     public class DichVuController : Controller
     {
+        private readonly QuanLyPhongTroContext _context;
+
+        public DichVuController(QuanLyPhongTroContext context)
+        {
+            _context = context;
+        }
+       
         public IActionResult Index()
         {
             return View();
