8606831129915a7f9a98d96a75156f3201573b6c
Bnseaaaa <haibuiboy@gmail.com>
2025-10-09 09:43:48 +0700

    xong dich vu

--- QuanLyPhongTro/Areas/QuanLy/Controllers/DichVuController.cs
+++ QuanLyPhongTro/Areas/QuanLy/Controllers/DichVuController.cs
@@ -1,12 +1,75 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyPhongTro.Models;
 
 namespace QuanLyPhongTro.Areas.QuanLy.Controllers
 {
+    [Area("QuanLy")]
     public class DichVuController : Controller
     {
-        public IActionResult Index()
+        private readonly QuanLyPhongTroContext _context;
+
+        public DichVuController(QuanLyPhongTroContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(DichVu model)
+        {
+            if (string.IsNullOrWhiteSpace(model.TenDv) || model.DonGia <= 0)
+            {
+                return Json(new { success = false, message = "Dữ liệu không hợp lệ!" });
+            }
+
+            _context.DichVus.Add(model);
+            _context.SaveChanges();
+
+            return Json(new
+            {
+                success = true,
+                data = new
+                {
+                    MaDv = model.MaDv,
+                    TenDv = model.TenDv,
+                    DonGia = model.DonGia.ToString("N0")
+                }
+            });
+        }
+
+        [HttpGet]
+        public IActionResult ReloadPartial()
+        {
+            return ViewComponent("DichVu");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int MaDV, string TenDv, decimal DonGia)
+        {
+            var item = _context.DichVus.Find(MaDV);
+            if (item == null) return NotFound("Không tìm thấy dịch vụ này.");
+
+            item.TenDv = TenDv;
+            item.DonGia = DonGia;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int maDv)
+        {
+            var item = _context.DichVus.Find(maDv);
+            if (item == null) return NotFound("Không tìm thấy dịch vụ này");
+
+            _context.DichVus.Remove(item);
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
+
 }
---
+++ QuanLyPhongTro/Areas/QuanLy/ViewComponents/DichVuViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyPhongTro.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuanLyPhongTro.Areas.QuanLy.ViewComponents
+{
+    public class DichVuViewComponent : ViewComponent
+    {
+        private readonly QuanLyPhongTroContext _context;
+
+        public DichVuViewComponent(QuanLyPhongTroContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //Lay danh sach dich vu
+            var dichVuList = _context.DichVus.ToList();
+
+            //truyen sang view
+            ViewBag.DichVuList = dichVuList.Where(tc => tc.TenDv != null).ToList();
+
+
+            return View("~/Areas/QuanLy/Views/DichVu/Index.cshtml");
+        }
+    }
+}
+
+
